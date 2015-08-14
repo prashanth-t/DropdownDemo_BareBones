846eb13cc55e7843457c6d4aae9c94a10dfd1d53
prashanth-t <prashanth2005@gmail.com>
2015-08-15 07:12:23 +1000

    initial commit

---
+++ DropdownDemo_BareBones/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using DropdownDemo_BareBones.Models;
+
+namespace DropdownDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "This is the index page";
+
+            return View();
+        }
+
+        public ActionResult MyView()
+        {
+            ViewBag.Message = "This is the MyView page";
+
+            TestModel m = new TestModel();
+            m.Gender = "Female";
+            return View(m);
+        }
+    }
+}
---
+++ DropdownDemo_BareBones/Models/TestModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DropdownDemo_BareBones.Models
+{
+    public class TestModel
+    {
+        public string Gender { get; set; }
+
+        public IEnumerable<SelectListItem> GenderList
+        {
+            get
+            {
+                List<SelectListItem> list = new List<SelectListItem> { new SelectListItem() { Text = "Select", Value = "Select" }, new SelectListItem() { Text = "Male", Value = "Male" }, new SelectListItem() { Text = "Female", Value = "Female" }, new SelectListItem() { Text = "Other", Value = "Other" } };
+                return list.Select(l => new SelectListItem { Selected = (l.Value == Gender), Text = l.Text, Value = l.Value });
+            }
+        }
+    }
+}
